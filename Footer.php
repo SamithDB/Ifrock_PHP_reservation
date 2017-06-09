300988772559fb480ef1e3b60be4f461e4582904
Sahan Monaara <sahanmonaara2015@gmail.com>
2017-06-09 14:47:11 +0530

    Add files via upload

---
+++ Footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Footer</title>
+  </head>
+  <body>
+    <?php
+
+    <div class="copywrite">
+     <div class="container">
+         <p>Copyright © 2017 iFrock. All Rights Reserved </a></p>
+       </div>
+    </div>
+
+
+    ?>
+  </body>
+</html>
